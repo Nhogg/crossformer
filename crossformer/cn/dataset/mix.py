94d5b07ac3f43840586e70279a6c4e173faf63ac
Nathan Hogg <nathanhogg1223@gmail.com>
2026-05-15 18:49:30 +0000

    bump lift ver

--- crossformer/cn/dataset/mix.py
+++ crossformer/cn/dataset/mix.py
@@ -175,7 +175,7 @@ class MultiDataSource(DataSource):
 _ = (TFDS(name="xgym_duck_single", head=Head.SINGLE, embodiment=SINGLE),)
 
 XGYM = [
-    Arec(name="xgym_lift_single", head=Head.SINGLE, embodiment=SINGLE, version="0.5.7", branch="main"),
+    Arec(name="xgym_lift_single", head=Head.SINGLE, embodiment=SINGLE, version="0.5.8", branch="main"),
     Arec(name="xgym_stack_single", head=Head.SINGLE, embodiment=SINGLE, version="0.5.5", branch="main"),
     Arec(name="xgym_sweep_single", head=Head.SINGLE, embodiment=SINGLE, version="0.6.0", branch="main"),
     Arec(name="sweep_mano", head=Head.MANO, embodiment=HUMAN_SINGLE, version="0.0.2", branch="to_step"),
@@ -203,6 +203,7 @@ MultiDataSource(
     data=XGYM,
     weights=XGYM_WEIGHTS,
 )
+
 
 sweep = [DataSource.REGISTRY["xgym_sweep_single"], DataSource.REGISTRY["sweep_mano"]]
 MultiDataSource(
